05544b2722ca9565162141b71007538591a7ebbe
Joshua Hunter <62202697+y4m7sdc36171vjm20wev@users.noreply.github.com>
2020-04-07 21:33:44 -0400

    Update search.js

--- search.js
+++ search.js
@@ -8,25 +8,25 @@
 
 const searchForID = (phrase) => {
     let arr = [];
-    for(let i = 1; i < document.body.innerHTML.split('id="').length; i++){
-        if(document.body.innerHTML.split('id="')[i].split('">')[0].search(phrase) != -1){
+    for (let i = 1; i < document.body.innerHTML.split('id="').length; i++) {
+        if (document.body.innerHTML.split('id="')[i].split('">')[0].search(phrase) != -1) {
             arr.push(document.body.innerHTML.split('id="')[i].split('"')[0]);
         }
     }
-    for(let i = 1; i < document.body.innerHTML.split("id='").length; i++){
-        if(document.body.innerHTML.split("id='")[i].split("'>")[0].search(phrase) != -1){
+    for (let i = 1; i < document.body.innerHTML.split("id='").length; i++) {
+        if (document.body.innerHTML.split("id='")[i].split("'>")[0].search(phrase) != -1) {
             arr.push(document.body.innerHTML.split("id='")[i].split("'")[0]);
         }
     }
     return arr;
 }
-const searchFrameForID = (phrase, frame) =>{
+const searchFrameForID = (phrase, frame) => {
     let results = [];
-    if(eval(frame) == undefined){
+    if (eval(frame) == undefined) {
         console.log("sorry but that frame doesn't exist.");
-    }else{
+    } else {
 
-    eval(`
+        eval(`
 
     let arr = [];
     for(let i = 1; i < ${frame}.document.body.innerHTML.split('id="').length; i++){
@@ -44,32 +44,32 @@ const searchFrameForID = (phrase, frame) =>{
 
     `);
 
-    return results;
-}
+        return results;
+    }
 }
 
 const searchForClass = (phrase) => {
     let arr = [];
-    for(let i = 1; i < document.body.innerHTML.split('class="').length; i++){
-        if(document.body.innerHTML.split('class="')[i].split('"')[0].search(phrase) != -1){
+    for (let i = 1; i < document.body.innerHTML.split('class="').length; i++) {
+        if (document.body.innerHTML.split('class="')[i].split('"')[0].search(phrase) != -1) {
             arr.push(document.body.innerHTML.split('class="')[i].split('"')[0]);
         }
     }
-    for(let i = 1; i < document.body.innerHTML.split("class='").length; i++){
-        if(document.body.innerHTML.split("class='")[i].split("'")[0].search(phrase) != -1){
+    for (let i = 1; i < document.body.innerHTML.split("class='").length; i++) {
+        if (document.body.innerHTML.split("class='")[i].split("'")[0].search(phrase) != -1) {
             arr.push(document.body.innerHTML.split("class='")[i].split("'")[0]);
         }
     }
     return arr;
 }
-const searchFrameForClass = (phrase, frame) =>{
+const searchFrameForClass = (phrase, frame) => {
     let results = [];
 
-    if(eval(frame) == undefined){
+    if (eval(frame) == undefined) {
         console.log("sorry but that frame doesn't exist.");
-    }else{
+    } else {
 
-    eval(`
+        eval(`
 
     let arr = [];
     for(let i = 1; i < ${frame}.document.body.innerHTML.split('class="').length; i++){
@@ -87,131 +87,164 @@ const searchFrameForClass = (phrase, frame) =>{
 
     `);
 
-    return results;
+        return results;
     }
 }
 
-const search = (phrase, frame, option, func) =>{
+const search = (phrase, frame, option, func) => {
 
-let allResults = [];
-let combine = "";
+    let allResults = [];
+    let combine = "";
 
-if(frame == undefined){
-    combine = searchForID(phrase).join("@") + "@" + searchForClass(phrase).join("@");
-}
-
-if(option == undefined){
-    if(frame != undefined && frame != null){
-        combine += "@" + searchFrameForClass(`"${phrase}"`, `${frame}`).join("@") + "@" + searchFrameForID(`"${phrase}"`,`${frame}`).join("@");
+    if (frame == undefined) {
+        combine = searchForID(phrase).join("@") + "@" + searchForClass(phrase).join("@");
     }
 
-}else{
-    if(option == 0){
-        if(frame == undefined){
-        combine = searchForID(phrase).join("@") + "@";
+    if (option == undefined) {
+        if (frame != undefined && frame != null) {
+            combine += "@" + searchFrameForClass(`"${phrase}"`, `${frame}`).join("@") + "@" + searchFrameForID(`"${phrase}"`, `${frame}`).join("@");
         }
-        if(frame != undefined && frame != null){
-            combine += "@" + searchFrameForID(`"${phrase}"`,`${frame}`).join("@");
-        }
-    }
-    if(option == 1){
-        if(frame == undefined){
-        combine = searchForClass(phrase).join("@") + "@";
-        } 
-        if(frame != undefined && frame != null){
-            combine += "@" + searchFrameForClass(`"${phrase}"`, `${frame}`).join("@");
-        }  
-    }
-    if(option > 1){
-        combine = "Choose 0 for searching 'id' values and 1 for 'class' values.";
-    }
-}
 
-allResults.push(combine);
-allResults = allResults[0].split("@");
-
-let check = "document";
-
-if(frame != undefined && frame != null){
-    check = `${frame}.document`;
-}
-
-allResults = allResults.sort(function(a,b){
-    return a.length - b.length;
-});
-
-allResults = allResults.filter(function(e){ return e != "" });
-
-if(option != undefined){
-    if(option == 0){
-        for(let j = 0; j < allResults.length; j++){
-            let a = `${check}.querySelectorAll("#"+allResults[${j}])`;
-            if(eval(a) == null){
-    
-                allResults[j] = "";
-            
-            }else{
-                if(func != undefined){
-
-                    if(func == "click"){
-                        eval(a + '.click()');
-                    }
-                    if(func == "getText"){
-                        eval(`console.log(${a}[${j}].innerText)`)
-                    }
-                    if(func.search("setText") != -1){
-                        eval(`${a}[${j}].innerText = "${func.split("@")[1]}"`)
-                    }
-                    if(func == "item"){
-                        return eval(a);
-                    }
-                    if(func.search(`fun`) != -1){
-                        function run(){
-                            let item = eval(`${a}[${j}]`);
-                            eval(`${func.split("@")[1]}`);
-                        }
-                        run();
-                    }
-                }
+    } else {
+        if (option == 0) {
+            if (frame == undefined) {
+                combine = searchForID(phrase).join("@") + "@";
+            }
+            if (frame != undefined && frame != null) {
+                combine += "@" + searchFrameForID(`"${phrase}"`, `${frame}`).join("@");
             }
         }
+        if (option == 1) {
+            if (frame == undefined) {
+                combine = searchForClass(phrase).join("@") + "@";
+            }
+            if (frame != undefined && frame != null) {
+                combine += "@" + searchFrameForClass(`"${phrase}"`, `${frame}`).join("@");
+            }
+        }
+        if (option > 2) {
+            combine = "Choose 0 for searching 'id' values and 1 for 'class' values.";
+        }
     }
-    if(option == 1){
-        for(let j = 0; j < allResults.length; j++){
-            let a = `${check}.getElementsByClassName(allResults[${j}])`;
-            
-            if(eval(a+'.length') == 0){
+
+    allResults.push(combine);
+    allResults = allResults[0].split("@");
+
+    let check = "document";
+
+    if (frame != undefined && frame != null) {
+        check = `${frame}.document`;
+    }
+
+    allResults = allResults.sort(function (a, b) {
+        return a.length - b.length;
+    });
+
+    allResults = allResults.filter(function (e) { return e != "" });
+
+    if (option != undefined) {
+        if (option == 0) {
+            for (let j = 0; j < allResults.length; j++) {
+                let a = `${check}.getElementById(allResults[${j}])`;
+                if (eval(a) == null) {
+
                     allResults[j] = "";
-            }else{
-                if(func != undefined){
-                    if(func == "click"){
-                        eval(a + '[0].click()');
-                    }
-                    if(func == "getText"){
-                        eval(`console.log(${a}[${j}].innerText)`)
-                    }
-                    if(func.search("setText") != -1){
-                        eval(`${a}[${j}].innerText = "${func.split("@")[1]}"`)
-                    }
-                    if(func == "item"){
-                            return eval(a);
-                    }
-                    if(func.search(`fun`) != -1){
-                        function run(){
-                            let item = eval(`${a}[${j}]`);
-                            eval(`${func.split("@")[1]}`);
-                            return true;
+
+                } else {
+                    if (func != undefined) {
+
+                        if (func == "click") {
+                            eval(a + '.click()');
                         }
-                        run();
+                        if (func == "getText") {
+                            eval(`console.log(${a}.innerText)`)
+                        }
+                        if (func.search("setText") != -1) {
+                            eval(`${a}.innerText = "${func.split("@")[1]}"`)
+                        }
+                        if (func == "item") {
+                            return eval(a);
+                        }
+                        if (func.search(`fun`) != -1) {
+                            function run() {
+                                let item = eval(a);
+                                eval(`${func.split("@")[1]}`);
+                            }
+                            run();
+                        }
                     }
                 }
             }
         }
+        if (option == 1) {
+            for (let j = 0; j < allResults.length; j++) {
+                let a = `${check}.getElementsByClassName(allResults[${j}])`;
+
+                if (eval(a + '.length') == 0) {
+                    allResults[j] = "";
+                } else {
+                    if (func != undefined) {
+                        if (func == "click") {
+                            eval(a + '[0].click()');
+                        }
+                        if (func == "getText") {
+                            eval(`console.log(${a}[${j}].innerText)`)
+                        }
+                        if (func.search("setText") != -1) {
+                            eval(`${a}[${j}].innerText = "${func.split("@")[1]}"`)
+                        }
+                        if (func == "item") {
+                            return eval(a);
+                        }
+                        if (func.search(`fun`) != -1) {
+                            function run() {
+                                let item = eval(`${a}[${j}]`);
+                                eval(`${func.split("@")[1]}`);
+                                return true;
+                            }
+                            run();
+                        }
+                    }
+                }
+            }
+        }
+        if (option == 2) {
+            for (let j = 0; j < allResults.length; j++) {
+                let a = `${check}.querySelectorAll("#"+allResults[${j}])`;
+                if (eval(a) == null) {
+
+                    allResults[j] = "";
+
+                } else {
+                    if (func != undefined) {
+
+                        if (func == "click") {
+                            eval(a + '.click()');
+                        }
+                        if (func == "getText") {
+                            eval(`console.log(${a}[${j}].innerText)`)
+                        }
+                        if (func.search("setText") != -1) {
+                            eval(`${a}[${j}].innerText = "${func.split("@")[1]}"`)
+                        }
+                        if (func == "item") {
+                            return eval(a);
+                        }
+                        if (func.search(`fun`) != -1) {
+                            function run() {
+                                let item = eval(a);
+                                eval(`${func.split("@")[1]}`);
+                            }
+                            run();
+                        }
+                    }
+                }
+            }
+        }
+
     }
 
-}
-
-return allResults;
+    return allResults;
 
 }
 
